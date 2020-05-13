635bff1fc209bcc85abd156799de698410df0193
sft940006 <sft940006@internet.local>
2020-05-13 13:53:53 +0430

    First commit

---
+++ EDMXEditor/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace EDMXEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonEDMX = new System.Windows.Forms.Button();
+            this.textBoxEdmx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxEdmxNameSpace = new System.Windows.Forms.TextBox();
+            this.buttonDiagram = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonEDMX
+            // 
+            this.buttonEDMX.Location = new System.Drawing.Point(43, 26);
+            this.buttonEDMX.Name = "buttonEDMX";
+            this.buttonEDMX.Size = new System.Drawing.Size(96, 32);
+            this.buttonEDMX.TabIndex = 0;
+            this.buttonEDMX.Text = "EDMX";
+            this.buttonEDMX.UseVisualStyleBackColor = true;
+            this.buttonEDMX.Click += new System.EventHandler(this.buttonEDMX_Click);
+            // 
+            // textBoxEdmx
+            // 
+            this.textBoxEdmx.Location = new System.Drawing.Point(145, 33);
+            this.textBoxEdmx.Name = "textBoxEdmx";
+            this.textBoxEdmx.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEdmx.TabIndex = 1;
+            this.textBoxEdmx.Text = "_d";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EDMX NameSpace";
+            // 
+            // textBoxEdmxNameSpace
+            // 
+            this.textBoxEdmxNameSpace.Location = new System.Drawing.Point(377, 33);
+            this.textBoxEdmxNameSpace.Name = "textBoxEdmxNameSpace";
+            this.textBoxEdmxNameSpace.Size = new System.Drawing.Size(308, 20);
+            this.textBoxEdmxNameSpace.TabIndex = 3;
+            this.textBoxEdmxNameSpace.Text = "http://schemas.microsoft.com/ado/2009/11/edmx";
+            // 
+            // buttonDiagram
+            // 
+            this.buttonDiagram.Location = new System.Drawing.Point(43, 75);
+            this.buttonDiagram.Name = "buttonDiagram";
+            this.buttonDiagram.Size = new System.Drawing.Size(96, 23);
+            this.buttonDiagram.TabIndex = 4;
+            this.buttonDiagram.Text = "Diagram";
+            this.buttonDiagram.UseVisualStyleBackColor = true;
+            this.buttonDiagram.Click += new System.EventHandler(this.buttonDiagram_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDiagram);
+            this.Controls.Add(this.textBoxEdmxNameSpace);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEdmx);
+            this.Controls.Add(this.buttonEDMX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonEDMX;
+        private System.Windows.Forms.TextBox textBoxEdmx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxEdmxNameSpace;
+        private System.Windows.Forms.Button buttonDiagram;
+    }
+}
+
---
+++ EDMXEditor/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace EDMXEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEDMX_Click(object sender, EventArgs e)
+        {
+            var r = openFileDialog1.ShowDialog();
+            if (r == DialogResult.Cancel)
+                return;
+            var f = openFileDialog1.FileName;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(f);
+
+            var nsmgr = new XmlNamespaceManager(doc.NameTable);
+            nsmgr.AddNamespace("edmx", textBoxEdmxNameSpace.Text);
+
+            // get a list of nodes - in this case, I'm selecting all <AID> nodes under
+            // the <GroupAIDs> node - change to suit your needs
+            XmlNodeList a = doc.SelectNodes("//edmx:Edmx/edmx:Runtime/edmx:ConceptualModels", nsmgr);
+            XmlNodeList aNodes = a[0].ChildNodes[0].ChildNodes;
+            // loop through all AID nodes
+            ConceptualModelsPostfix(aNodes);
+
+
+            XmlNodeList b = doc.SelectNodes("//edmx:Edmx/edmx:Runtime/edmx:Mappings", nsmgr);
+            XmlNodeList bNodes = b[0].ChildNodes[0].ChildNodes[0].ChildNodes;
+            MappingsPostfix(bNodes);
+
+            doc.Save(f);
+            MessageBox.Show("Finish");
+        }
+
+        void MappingsPostfix(XmlNodeList bNodes)
+        {
+            foreach (XmlNode bNode in bNodes)
+            {
+                if(bNode.Name== "EntitySetMapping")
+                {
+                    XmlAttribute Name = bNode.Attributes["Name"];
+                    Name.Value = Name.Value + textBoxEdmx.Text;
+
+                    foreach (XmlNode EntityTypeMapping in bNode.ChildNodes)
+                    {
+                        if (EntityTypeMapping.Name == "EntityTypeMapping")
+                        {
+                            XmlAttribute TypeName = EntityTypeMapping.Attributes["TypeName"];
+                            TypeName.Value = TypeName.Value + textBoxEdmx.Text;
+                        }
+                    }
+                }
+                else if (bNode.Name== "FunctionImportMapping")
+                {
+                    XmlAttribute FunctionImportName = bNode.Attributes["FunctionImportName"];
+                    FunctionImportName.Value = FunctionImportName.Value + textBoxEdmx.Text;                      
+                            
+                    if (bNode.ChildNodes.Count > 0)
+                    {
+                        foreach (XmlNode ComplexTypeMapping in bNode.ChildNodes[0].ChildNodes)
+                        {
+                            XmlAttribute TypeName = ComplexTypeMapping.Attributes["TypeName"];
+                            TypeName.Value = TypeName.Value + textBoxEdmx.Text;
+                        }
+                    }          
+                }
+            }
+        }
+
+        void ConceptualModelsPostfix(XmlNodeList aNodes)
+        {
+            foreach (XmlNode aNode in aNodes)
+            {
+                if (aNode.Name == "EntityType" || aNode.Name == "Association" || aNode.Name == "ComplexType")
+                {
+                    XmlAttribute idAttribute = aNode.Attributes["Name"];
+                    idAttribute.Value = idAttribute.Value + textBoxEdmx.Text;
+                    foreach (XmlNode End in aNode.ChildNodes)
+                    {
+                        if (End.Name == "NavigationProperty")
+                        {
+                            XmlAttribute Relationship = End.Attributes["Relationship"];
+                            Relationship.Value = Relationship.Value + textBoxEdmx.Text;
+                        }
+                    }
+                }
+                if (aNode.Name == "Association")
+                {
+                    foreach (XmlNode End in aNode.ChildNodes)
+                    {
+                        if (End.Name == "End")
+                        {
+                            XmlAttribute tType = End.Attributes["Type"];
+                            tType.Value = tType.Value + textBoxEdmx.Text;
+                        }
+                    }
+                }
+
+                if (aNode.Name == "EntityContainer")
+                {
+                    foreach (XmlNode End in aNode.ChildNodes)
+                    {
+                        if (End.Name == "EntitySet")
+                        {
+                            XmlAttribute EntityType = End.Attributes["EntityType"];
+                            EntityType.Value = EntityType.Value + textBoxEdmx.Text;
+
+                            XmlAttribute Name = End.Attributes["Name"];
+                            Name.Value = Name.Value + textBoxEdmx.Text;
+
+                        }
+                        else if (End.Name == "AssociationSet")
+                        {
+                            XmlAttribute Name = End.Attributes["Name"];
+                            Name.Value = Name.Value + textBoxEdmx.Text;
+
+                            XmlAttribute Association = End.Attributes["Association"];
+                            Association.Value = Association.Value + textBoxEdmx.Text;                            
+
+                            foreach (XmlNode e in End.ChildNodes)
+                            {
+                                XmlAttribute EntitySet = e.Attributes["EntitySet"];
+                                EntitySet.Value = EntitySet.Value + textBoxEdmx.Text;                                
+                            }
+                        }
+                        else if(End.Name== "FunctionImport")
+                        {
+                            XmlAttribute Name = End.Attributes["Name"];
+                            Name.Value = Name.Value + textBoxEdmx.Text;
+
+                            XmlAttribute ReturnType = End.Attributes["ReturnType"];
+                            if (ReturnType != null)
+                            {
+                                if (ReturnType.Value.Length > "Collection(Double)".Length+5)
+                                {
+                                    ReturnType.Value = ReturnType.Value.Replace(")", textBoxEdmx.Text+")");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonDiagram_Click(object sender, EventArgs e)
+        {
+            var r=openFileDialog1.ShowDialog();
+            if (r == DialogResult.Cancel)
+                return;
+            var f = openFileDialog1.FileName;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(f);
+
+            var nsmgr = new XmlNamespaceManager(doc.NameTable);
+            nsmgr.AddNamespace("edmx", textBoxEdmxNameSpace.Text);
+
+            // get a list of nodes - in this case, I'm selecting all <AID> nodes under
+            // the <GroupAIDs> node - change to suit your needs
+            XmlNodeList a = doc.SelectNodes("//edmx:Edmx/edmx:Designer/edmx:Diagrams", nsmgr);
+            XmlNodeList Diagram = a[0].ChildNodes[0].ChildNodes;
+            foreach (XmlNode aNode in Diagram)
+            {
+                if(aNode.Name== "EntityTypeShape")
+                {
+                    XmlAttribute EntityType = aNode.Attributes["EntityType"];
+                    EntityType.Value = EntityType.Value + textBoxEdmx.Text;
+                    
+                }
+                else if (aNode.Name == "AssociationConnector")
+                {
+                    XmlAttribute Association = aNode.Attributes["Association"];
+                    Association.Value = Association.Value + textBoxEdmx.Text;                    
+                }
+            }
+
+            doc.Save(f);
+            MessageBox.Show("Finish");
+        }
+    }
+}
